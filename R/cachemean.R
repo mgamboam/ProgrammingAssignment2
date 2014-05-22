a5eccda418a19d4332fad2f7e6392562940d5f41
Mario Gamboa <mgamboam@outlook.com>
2014-05-21 23:56:13 -0600

    Final

--- R/cachemean.R
+++ R/cachemean.R
@@ -2,14 +2,27 @@
 ## Creator: Mario Gamboa
 ## Reference: Taken from: https://class.coursera.org/rprog-003/human_grading/view/courses/972138/assessments/3/submissions
 
-#' \code{makeVector} creates a special "vector", which is a list containing a functions to:
-#' * set the value of the vector
-#' * get the value of the vector
-#' * set the value of the mean
-#' * get the value of the mean
+#' Special Vector object with cache
 #' 
-#' @param x the vector to which the mean function will be calculated for
-#' @return a list of functions associated with the vector
+#' \code{makeVector} Creates a vector object with cache features
+#' 
+#' @details
+#' creates a special "vector", which is a list containing a functions to:
+#' \itemize{
+#'    \item set the value of the vector
+#'    \item get the value of the vector
+#'    \item set the value of the mean
+#'    \item get the value of the mean
+#' }
+#'    
+#' @param x vector to which the mean function will be calculated for
+#'  @export
+#' @author Mario Gamboa
+#' @keywords cache
+#' @export
+#' @examples
+#' x <- makeVector(c(10,20,5,18))
+#' x$get()
 makeVector <- function(x = numeric()) {
       m <- NULL
       set <- function(y) {
@@ -24,11 +37,22 @@ makeVector <- function(x = numeric()) {
            getmean = getmean)
 }
 
-
-#' \code{cachemean} function that computes the mean of a vector only if the value is not in cache
+#' Mean of Vector with cache
 #' 
-#' @param x the vector to which the mean function will be calculated for
-#' @return the mean value for the vector
+#' \code{cachemean} Computes the mean of a vector only if the value is not in cache
+#' 
+#' @details
+#' The method intelligently uses cached data to avoid re-calculation
+#'  
+#' @param x vector to which the mean function will be calculated for
+#' @param ... additional arguments to the regular \code{mean} function
+#' @export
+#' @author Mario Gamboa
+#' @keywords cache
+#' @export
+#' @examples
+#' x <- makeVector(c(10,20,5,18))
+#' cachemean(x)
 cachemean <- function(x, ...) {
       m <- x$getmean()
       if(!is.null(m)) {
